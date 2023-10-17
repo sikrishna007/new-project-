07e2b9efb7780a222f209949a44afff6227ad04a
Girish Kumar <girishkumargk7480@gmail.com>
2023-10-17 15:15:01 +0530

    Removed Uppercase condition for names

--- src/custom-components/productTable.js
+++ src/custom-components/productTable.js
@@ -277,7 +277,7 @@ export const ProductTable = (props) => {
                                                     }
                                                     variant="subtitle2"
                                                 >
-                                                    {product?.name && product.name.charAt(0).toUpperCase() + product.name.slice(1)}
+                                                    {product?.name}
 
                                                 </Link>
                                             </div>
@@ -297,17 +297,12 @@ export const ProductTable = (props) => {
                                         </Typography>
                                     </TableCell>
                                     <TableCell sx={{textAlign: "left"}}>
-                                        <Typography variant="subtitle2">
                                             ₹ {product?.unitPrice}
-                                        </Typography>
                                     </TableCell>
                                     <TableCell sx={{textAlign: "left"}}>
-                                        <Typography variant="subtitle2">
                                             {getVendorNameById(product?.vendor)}
-                                        </Typography>
                                     </TableCell>
                                     <TableCell sx={{textAlign: "left"}}>
-                                        <Typography variant="subtitle2">
                                             {new Date(product?.createdAt).toLocaleDateString(undefined, {
                                                 day: "2-digit",
                                                 month: "short",
@@ -315,13 +310,11 @@ export const ProductTable = (props) => {
                                                 hour: "2-digit",
                                                 minute: "2-digit"
                                             })}
-                                        </Typography>
                                     </TableCell>
                                     {/*<Typography variant="subtitle2">*/}
                                     {/*    {product?.updatedBy}*/}
                                     {/*</Typography>*/}
                                     <TableCell sx={{textAlign: "left"}}>
-                                        <Typography variant="subtitle2">
                                             {new Date(product?.updatedAt).toLocaleDateString(undefined, {
                                                 day: "2-digit",
                                                 month: "short",
@@ -329,7 +322,6 @@ export const ProductTable = (props) => {
                                                 hour: "2-digit",
                                                 minute: "2-digit"
                                             })}
-                                        </Typography>
                                     </TableCell>
                                     <TableCell sx={{textAlign: "left"}}>
                                         {product?.isActive ? (
