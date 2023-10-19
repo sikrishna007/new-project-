d0d58c44e8cb5e9082acf49fa57ff46b6227e836
ravi kiran jalli <ravikiran.jalli26@gmail.com>
2023-10-19 19:13:11 +0530

    Added Authorization for every role

--- src/layouts/admin-dashboard/config.js
+++ src/layouts/admin-dashboard/config.js
@@ -11,184 +11,351 @@ import Cookies from "js-cookie";
 
 export const useSections = () => {
   const { t } = useTranslation();
+  // <RoleBasedGuard permissions={["ADMIN","VENDOR","VENDOR MANAGER","SALES MANAGER","EVENT MANAGER","MARKETING","ACCOUNTANT"]} >
 
   return useMemo(() => {
     let role = Cookies.get("role")
-    if(role !== `VENDOR`) {
-      return [
-        {
-          subheader: "Admin Panel",
+    switch (role){
+      case 'ADMIN':
+        return [
+          {
+            subheader: "Admin Panel",
 
-          items: [
-            {
-              title: t(tokens.nav.dashboard),
-              path: paths.dashboard.index,
-              icon: (
-                  <SvgIcon fontSize="small">
-                    <HomeSmileIcon/>
-                  </SvgIcon>
-              ),
-            },
+            items: [
+              {
+                title: t(tokens.nav.dashboard),
+                path: paths.dashboard.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <HomeSmileIcon/>
+                    </SvgIcon>
+                ),
+              },
 
-            {
-              title: t(tokens.nav.userManagement),
-              path: paths.userManagement.index,
-              icon: (
-                  <SvgIcon fontSize="small">
-                    <Users03Icon/>
-                  </SvgIcon>
-              ),
-              items: [
-                {
-                  title: t(tokens.nav.employees),
-                  path: paths.userManagement.employees.index,
-                  icon: (
-                      <SvgIcon fontSize="small">
-                        <Users03Icon/>
-                      </SvgIcon>
-                  ),
-                },
-                {
-                  title: t(tokens.nav.customers),
-                  path: paths.userManagement.customers.index,
-                  icon: (
-                      <SvgIcon fontSize="small">
-                        <Users03Icon/>
-                      </SvgIcon>
-                  ),
-                },
-                {
-                  title: t(tokens.nav.vendors),
-                  path: paths.userManagement.vendors.index,
-                  icon: (
-                      <SvgIcon fontSize="small">
-                        <Users03Icon/>
-                      </SvgIcon>
-                  ),
-                },
-              ],
-            },
-            {
-              title: t(tokens.nav.eventCategory),
-              path: paths.eventCategory.index,
-              icon: (
-                  <SvgIcon fontSize="small">
-                    <CategoryOutlined/>
-                  </SvgIcon>
-              ),
-            },
+              {
+                title: t(tokens.nav.userManagement),
+                path: paths.userManagement.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <Users03Icon/>
+                    </SvgIcon>
+                ),
+                items: [
+                  {
+                    title: t(tokens.nav.employees),
+                    path: paths.userManagement.employees.index,
+                    icon: (
+                        <SvgIcon fontSize="small">
+                          <Users03Icon/>
+                        </SvgIcon>
+                    ),
+                  },
+                  {
+                    title: t(tokens.nav.customers),
+                    path: paths.userManagement.customers.index,
+                    icon: (
+                        <SvgIcon fontSize="small">
+                          <Users03Icon/>
+                        </SvgIcon>
+                    ),
+                  },
+                  {
+                    title: t(tokens.nav.vendors),
+                    path: paths.userManagement.vendors.index,
+                    icon: (
+                        <SvgIcon fontSize="small">
+                          <Users03Icon/>
+                        </SvgIcon>
+                    ),
+                  },
+                ],
+              },
+              {
+                title: t(tokens.nav.eventCategory),
+                path: paths.eventCategory.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <CategoryOutlined/>
+                    </SvgIcon>
+                ),
+              },
 
-            {
-              title: t(tokens.nav.productManagement),
-              path: paths.productManagement.index,
-              icon: (
-                  <SvgIcon fontSize="small">
-                    <Inventory/>
-                  </SvgIcon>
-              ),
-              items: [
-                {
-                  title: t(tokens.nav.category),
-                  path: paths.productManagement.category.index,
-                },
-                {
-                  title: t(tokens.nav.subCategory),
-                  path: paths.productManagement.subCategory.index,
-                },
-                {
-                  title: t(tokens.nav.products),
-                  path: paths.productManagement.products.index,
-                },
-              ],
-            },
-            {
-              title: t(tokens.nav.kycVerification),
-              path: paths.kycVerification.index,
-              icon: (
-                  <SvgIcon fontSize="small">
-                    <VerifiedIcon/>
-                  </SvgIcon>
-              ),
-              items: [
-                {
-                  title: t(tokens.nav.businessCustomer),
-                  path: paths.kycVerification.businessCustomer.index,
-                  icon: (
-                      <SvgIcon fontSize="small">
-                        <VerifiedIcon/>
-                      </SvgIcon>
-                  ),
-                },
-                {
-                  title: t(tokens.nav.vendor),
-                  path: paths.kycVerification.vendor.index,
-                  icon: (
-                      <SvgIcon fontSize="small">
-                        <VerifiedIcon/>
-                      </SvgIcon>
-                  ),
-                },
+              {
+                title: t(tokens.nav.productManagement),
+                path: paths.productManagement.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <Inventory/>
+                    </SvgIcon>
+                ),
+                items: [
+                  {
+                    title: t(tokens.nav.category),
+                    path: paths.productManagement.category.index,
+                  },
+                  {
+                    title: t(tokens.nav.subCategory),
+                    path: paths.productManagement.subCategory.index,
+                  },
+                  {
+                    title: t(tokens.nav.products),
+                    path: paths.productManagement.products.index,
+                  },
+                ],
+              },
+              {
+                title: t(tokens.nav.kycVerification),
+                path: paths.kycVerification.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <VerifiedIcon/>
+                    </SvgIcon>
+                ),
+                items: [
+                  {
+                    title: t(tokens.nav.businessCustomer),
+                    path: paths.kycVerification.businessCustomer.index,
+                    icon: (
+                        <SvgIcon fontSize="small">
+                          <VerifiedIcon/>
+                        </SvgIcon>
+                    ),
+                  },
+                  {
+                    title: t(tokens.nav.vendor),
+                    path: paths.kycVerification.vendor.index,
+                    icon: (
+                        <SvgIcon fontSize="small">
+                          <VerifiedIcon/>
+                        </SvgIcon>
+                    ),
+                  },
 
-              ],
-            },
-            {
-              title: t(tokens.nav.setup),
-              path: paths.setup.index,
-              icon: (
-                  <SvgIcon fontSize="small">
-                    <Settings/>
-                  </SvgIcon>
-              ),
-              items: [
-                {
-                  title: t(tokens.nav.hsn),
-                  path: paths.setup.hsn.index,
-                  icon: (
-                      <SvgIcon fontSize="small">
-                        <Settings/>
-                      </SvgIcon>
-                  ),
-                },
-                {
-                  title: t(tokens.nav.sac),
-                  path: paths.setup.sac.index,
-                  icon: (
-                      <SvgIcon fontSize="small">
-                        <Settings/>
-                      </SvgIcon>
-                  ),
-                },
+                ],
+              },
+              {
+                title: t(tokens.nav.setup),
+                path: paths.setup.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <Settings/>
+                    </SvgIcon>
+                ),
+                items: [
+                  {
+                    title: t(tokens.nav.hsn),
+                    path: paths.setup.hsn.index,
+                    icon: (
+                        <SvgIcon fontSize="small">
+                          <Settings/>
+                        </SvgIcon>
+                    ),
+                  },
+                  {
+                    title: t(tokens.nav.sac),
+                    path: paths.setup.sac.index,
+                    icon: (
+                        <SvgIcon fontSize="small">
+                          <Settings/>
+                        </SvgIcon>
+                    ),
+                  },
 
-              ],
-            },
-          ],
-        },
-      ];
+                ],
+              },
+            ],
+          },
+        ];
+      case 'VENDOR':
+        return [
+          {
+            subheader: "Vendor Panel",
+            items: [
+              {
+                title: t(tokens.nav.dashboard),
+                path: paths.dashboard.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <HomeSmileIcon/>
+                    </SvgIcon>
+                ),
+              },
+              {
+                title: t(tokens.nav.productManagement),
+                path: paths.productManagement.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <Inventory/>
+                    </SvgIcon>
+                ),
+                items: [
+
+                  {
+                    title: t(tokens.nav.products),
+                    path: paths.productManagement.products.index,
+                  },
+                ],
+              }
+            ]
+          }
+        ];
+      case 'VENDOR MANAGER':
+        return [
+          {
+            subheader: "Vendor Manager Panel",
+            items: [
+              {
+                title: t(tokens.nav.dashboard),
+                path: paths.dashboard.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <HomeSmileIcon/>
+                    </SvgIcon>
+                ),
+              },
+              {
+                title: t(tokens.nav.productManagement),
+                path: paths.productManagement.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <Inventory/>
+                    </SvgIcon>
+                ),
+                items: [
+                  {
+                    title: t(tokens.nav.category),
+                    path: paths.productManagement.category.index,
+                  },
+                  {
+                    title: t(tokens.nav.subCategory),
+                    path: paths.productManagement.subCategory.index,
+                  },
+                  {
+                    title: t(tokens.nav.products),
+                    path: paths.productManagement.products.index,
+                  },
+                ],
+              },
+            ]
+          }
+        ];
+      case 'SALES MANAGER':
+        return [
+          {
+            subheader: "Sales Manager Panel",
+            items: [
+              {
+                title: t(tokens.nav.dashboard),
+                path: paths.dashboard.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <HomeSmileIcon/>
+                    </SvgIcon>
+                ),
+              },
+              {
+                title: t(tokens.nav.userManagement),
+                path: paths.userManagement.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <Users03Icon/>
+                    </SvgIcon>
+                ),
+                items: [
+                  {
+                    title: t(tokens.nav.customers),
+                    path: paths.userManagement.customers.index,
+                    icon: (
+                        <SvgIcon fontSize="small">
+                          <Users03Icon/>
+                        </SvgIcon>
+                    ),
+                  },
+                  {
+                    title: t(tokens.nav.vendors),
+                    path: paths.userManagement.vendors.index,
+                    icon: (
+                        <SvgIcon fontSize="small">
+                          <Users03Icon/>
+                        </SvgIcon>
+                    ),
+                  },
+                ],
+              },
+
+            ]
+          }
+        ];
+      case 'EVENT MANAGER':
+        return [
+          {
+            subheader: "Event Manager Panel",
+            items: [
+              {
+                title: t(tokens.nav.dashboard),
+                path: paths.dashboard.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <HomeSmileIcon/>
+                    </SvgIcon>
+                ),
+              },
+            ]
+          }
+        ];
+      case 'MARKETING':
+        return [
+          {
+            subheader: "Marketing Panel",
+            items: [
+              {
+                title: t(tokens.nav.dashboard),
+                path: paths.dashboard.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <HomeSmileIcon/>
+                    </SvgIcon>
+                ),
+              },
+            ]
+          }
+        ];
+      case 'ACCOUNTANT':
+        return [
+          {
+            subheader: "Accountant Panel",
+            items: [
+              {
+                title: t(tokens.nav.dashboard),
+                path: paths.dashboard.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <HomeSmileIcon/>
+                    </SvgIcon>
+                ),
+              },
+            ]
+          }
+        ];
+      case 'CUSTOMER SUPPORT':
+        return [
+          {
+            subheader: "Customer Support Panel",
+            items: [
+              {
+                title: t(tokens.nav.dashboard),
+                path: paths.dashboard.index,
+                icon: (
+                    <SvgIcon fontSize="small">
+                      <HomeSmileIcon/>
+                    </SvgIcon>
+                ),
+              },
+            ]
+          }
+        ];
     }
-    else {
-      return [
-        {
-          subheader: "Vendor Panel",
-          items: [
-            {
-              title: t(tokens.nav.productManagement),
-              path: paths.productManagement.index,
-              icon: (
-                  <SvgIcon fontSize="small">
-                    <Inventory/>
-                  </SvgIcon>
-              ),
-              items: [
 
-                {
-                  title: t(tokens.nav.products),
-                  path: paths.productManagement.products.index,
-                },
-              ],
-            }
-          ]
-        }
-      ]
-    }
+
   }, [t]);
 };
